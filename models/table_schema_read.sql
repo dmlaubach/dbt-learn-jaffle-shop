c2752c3bbc7b06bfba62a1f5fdeebdca1fd38810
Derek Laubach <dmlaubach@gmail.com>
2026-04-15 07:03:26 +0000

    added clean_slate macro and table_schema_read sql.

---
+++ models/table_schema_read.sql
@@ -0,0 +1,10 @@
+{% set database  = target.database %}
+{% set schema = target.schema %}
+
+select table_type,
+    table_schema,
+    table_name,
+    last_altered,
+from {{ database }}.information_schema.tables
+where table_schema = upper('{{ schema }}')
+order by 4 desc
